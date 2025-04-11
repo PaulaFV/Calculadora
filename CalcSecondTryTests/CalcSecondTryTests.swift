a3a5da961d1994c959ce87e2889acd01d5df017a
PaulaFVplx <paula.fernandezvazquez@plexus.es>
2025-04-11 12:58:46 +0200

    Initial Commit

---
+++ CalcSecondTryTests/CalcSecondTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalcSecondTryTests.swift
+//  CalcSecondTryTests
+//
+//  Created by Paula Fernández Vázquez on 11/4/25.
+//
+
+import Testing
+@testable import CalcSecondTry
+
+struct CalcSecondTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
